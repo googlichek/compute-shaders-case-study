e47ab8bde60f17cc3aa18f505acd745ef2f5104e
Goga Akobiya <googlichek.one@gmail.com>
2021-11-15 18:51:06 +0300

    Multiple kernels.

---
+++ Assets/Scripts/SolidColor.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class SolidColor : MonoBehaviour
+{
+    [SerializeField]
+    private string _kernelName = "SolidRed";
+
+    [SerializeField, Space]
+    private ComputeShader _shader = default;
+
+    [SerializeField]
+    private int _texResolution = 256;
+
+    [SerializeField]
+    private Renderer _renderer = default;
+
+    private RenderTexture _outputTexture = default;
+
+    private int _kernelHandle = 0;
+
+    void Start()
+    {
+        _outputTexture = new RenderTexture(_texResolution, _texResolution, 0);
+        _outputTexture.enableRandomWrite = true;
+        _outputTexture.Create();
+
+        _renderer.enabled = true;
+
+        InitShader();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            DispatchShader(_texResolution / 8, _texResolution / 8);
+        }
+    }
+
+    private void InitShader()
+    {
+        _kernelHandle = _shader.FindKernel(_kernelName);
+
+        _shader.SetInt("texResolution", _texResolution);
+        _shader.SetTexture(_kernelHandle, "Result", _outputTexture);
+
+        _renderer.material.SetTexture("_MainTex", _outputTexture);
+
+        DispatchShader(_texResolution / 16, _texResolution / 16);
+    }
+
+    private void DispatchShader(int x, int y)
+    {
+        _shader.Dispatch(_kernelHandle, x, y, 1);
+    }
+}
